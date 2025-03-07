fd1789218f0f4a998794a8ee2d23b9eef952b8da
Michael <michaelandresrr@gmail.com>
2025-03-06 19:00:37 -0500

    Add initial HTML, CSS, and JavaScript for Barrio La Coruña project

---
+++ inicializar_mapa.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var map1 = L.map('map1').setView([4.60971, -74.08175], 13);
+    var map2 = L.map('map2').setView([4.60971, -74.08175], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map1);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map2);
+
+    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map2);
+
+    map1.sync(map2);
+    map2.sync(map1);
+});
